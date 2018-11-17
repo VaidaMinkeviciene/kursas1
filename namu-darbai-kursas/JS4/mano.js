92ff38f28c933708bdc4967069ccdb53d4e86fec
VaidaMinkeviciene <vaida.vaimon@gmail.com>
2018-11-17 17:49:41 +0200

    JS4

---
+++ namu-darbai-kursas/JS4/mano.js
@@ -0,0 +1,12 @@
+function amziausCenzas() {
+  var age, x;
+  x = document.getElementById("age").value;
+
+  if(x>=18){
+
+  alert("Sveiki atvykę");
+  }
+  else {
+  alert(" Esate netinkamo amžiaus į svetainę užeiti negalite!");
+  }
+  }
